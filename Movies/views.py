38a197c617c62db197c1bcfc9490eaf59a1b5109
Diego lopez <lolo4814@hotmail.com>
2024-03-01 09:12:33 -0500

    se agrega saludo a la pagina

--- Movies/views.py
+++ Movies/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
+
+def saludo(request):
+    return HttpResponse("Hola, bienvenido a mi página")
 
 # Create your views here.
